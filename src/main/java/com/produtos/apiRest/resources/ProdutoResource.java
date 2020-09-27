1ac0a4ebe2ae15fa517c1c604001737f79eaaab1
Marycorreia12 <correia_nety@hotmail.com>
2020-09-27 17:56:06 +0100

    Implementing DELETE and PUT method

--- src/main/java/com/produtos/apiRest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apiRest/resources/ProdutoResource.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,14 +28,24 @@ public class ProdutoResource {
 		return produtoRepository.findAll();
 	}
 	
-	@GetMapping(value = "/produtos/{id}")
+	@GetMapping(value = "/produto/{id}")
 	public Optional<Produto> listaProdutoUnico(@PathVariable Long id){
 		return  produtoRepository.findById(id);
 	}
 	
 	
-	@PostMapping
+	@PostMapping(value = "/produto")
 	public Produto criarProduto(@RequestBody Produto produto){
+		return produtoRepository.save(produto);
+	}
+	
+	@DeleteMapping(value = "/produto")
+	public void deletarProduto(@RequestBody Produto produto){
+		produtoRepository.delete(produto);
+	}
+	
+	@PutMapping(value = "/produto")
+	public Produto atualizarProduto(@RequestBody Produto produto){
 		return produtoRepository.save(produto);
 	}
 
